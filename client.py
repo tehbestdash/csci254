37c5ec0fe12d2cc63dc6be91f34dea84ee3a62a1
tehbestdash <tehbestdash@gmail.com>
2015-12-08 17:26:41 -0800

    Update client.py

--- client.py
+++ client.py
@@ -8,7 +8,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
 server_address = ('localhost', 8000)
-print >>sys.stderr, 'starting up on %s port %s' % server_address
+print >>sys.stderr, 'starting up on %s port %s ' % server_address
 sock.connect(server_address)
 
 
